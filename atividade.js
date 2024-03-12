96abcc6c4a3a2bc91ee48116ad84b7eaf4cfaf25
Lucas Zani <zanin0503@gmail.com>
2024-03-12 09:35:29 -0300

    correção da palavra realizável da linha 12

--- atividade.js
+++ atividade.js
@@ -9,7 +9,7 @@ if (b % 1 != 0 || a % 1 != 0) {
         console.log(a+ "vezes"+ i+ "é igual a"+ i * a);
     }
 } else if (a == 0 || b == 0) {
-    console.log("operação não realizavel, pois algum valor é igual a 0");
+    console.log("operação não realizável, pois algum valor é igual a 0");
 } else {
     console.log("operação não realizavel, pois algum número é menor que 0");
 }
